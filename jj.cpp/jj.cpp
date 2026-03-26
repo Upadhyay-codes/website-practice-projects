4cf679eed0f50ceb5c38583d45fdf62a1659e1f1
Upadhyay-codes <abhishek24448@gmail.com>
2026-03-26 23:28:36 +0530

    c++ basics

---
+++ jj.cpp/jj.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int a =34;
+    char c ='c';
+    cout <<"the value of a was: " << a <<"\n";
+    a =45;
+    c = '4';
+    cout << "the valuse of a is:" << a;
+    return 0;
+}
